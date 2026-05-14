b02f256ba5c8040b79ccbab7f96cf82abbc8c8c7
Art Kay <art@artkay.net>
2026-05-13 22:46:08 -0400

    appease credo

--- lib/faker/gov/it.ex
+++ lib/faker/gov/it.ex
@@ -53,22 +53,9 @@ defmodule Faker.Gov.It do
     "#{year}#{month}#{day}"
   end
 
-  defp month_code(month) do
-    case month do
-      1 -> "A"
-      2 -> "B"
-      3 -> "C"
-      4 -> "D"
-      5 -> "E"
-      6 -> "H"
-      7 -> "L"
-      8 -> "M"
-      9 -> "P"
-      10 -> "R"
-      11 -> "S"
-      12 -> "T"
-    end
-  end
+  @month_codes {"A", "B", "C", "D", "E", "H", "L", "M", "P", "R", "S", "T"}
+
+  defp month_code(month), do: elem(@month_codes, month - 1)
 
   defp town_code do
     case Util.pick(~w(A B C D E F G H I L M Z)) do
